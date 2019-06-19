a3bb87f053665ef8aa0e1f79347d1c8216ce2190
NguyenBaoTrung04011996 <43132903+NguyenBaoTrung04011996@users.noreply.github.com>
2019-06-19 09:52:45 +0700

    Update testjs.js

--- testjs.js
+++ testjs.js
@@ -1,8 +1,13 @@
 
 
-  let button = document.getElementById('connect');
+  function loaded()
+ {
+ 	var button = document.getElementById('connect');	
+ 	button.onclick = click_bt;
+ }
+ 
 
- let click_bt()
+  function click_bt()
 {
      alert("baotrung");	
       //device = await navigator.usb.requestDevice(0x2341)
