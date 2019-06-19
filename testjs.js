ca0c0bdc154f790659564733b7606912852de3d3
NguyenBaoTrung04011996 <43132903+NguyenBaoTrung04011996@users.noreply.github.com>
2019-06-19 09:55:36 +0700

    Add files via upload

---
+++ testjs.js
@@ -0,0 +1,13 @@
+ function loaded()
+ {
+ 	var button = document.getElementById('connect');	
+ 	button.onclick = click_bt;
+ }
+ 
+
+  function click_bt()
+{
+     alert("baotrung");	
+      //device = await navigator.usb.requestDevice(0x2341)
+
+}     
