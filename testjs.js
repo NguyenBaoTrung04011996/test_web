245b4707bb43dc875436ba73f1b46943aaa59597
NguyenBaoTrung04011996 <43132903+NguyenBaoTrung04011996@users.noreply.github.com>
2019-06-19 09:12:38 +0700

    Update testjs.js

--- testjs.js
+++ testjs.js
@@ -1,1 +1,20 @@
 
+document.addEventListener('DOMContentLoaded', event => {
+  let button = document.getElementById('connect')
+
+  button.addEventListener('click', async() => {
+    let device
+   
+    
+    try {
+      device = await navigator.usb.requestDevice(0x2341)
+
+      console.log('open')
+      await device.open()
+      console.log('opened:', device)
+    } catch (error) {
+      console.log(error)
+    }
+	  await device.close()
+  })
+})
